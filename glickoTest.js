74f5152737c851bb90bcb956936d31918636b231
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-11 11:38:19 +0300

    variables name fix-2

--- glickoTest.js
+++ glickoTest.js
@@ -15,9 +15,9 @@ handlers.glickoTest = function (args, context)
 	matches.push([p1, p2, 1]);
 	glicko.updateRatings(matches);
 	
-	var rating = (Math.abs(Ryan.getRating() - 1464) < 0.1).should.be.true;
-	var rd = (Math.abs(Ryan.getRd() - 151.52) < 0.01).should.be.true;
-	var vol = (Math.abs(Ryan.getVol() - 0.05999) < 0.00001).should.be.true;
+	var rating = (Math.abs(p1.getRating() - 1464) < 0.1).should.be.true;
+	var rd = (Math.abs(p1.getRd() - 151.52) < 0.01).should.be.true;
+	var vol = (Math.abs(p1.getVol() - 0.05999) < 0.00001).should.be.true;
 	
 	return {rating: rating, rd: rd, vol:vol};
 }

2026ec65f319c5f3c2d516d7a1cbb9d26a56cb0a
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-15 20:56:54 +0300

    invite friend

---
+++ inviteFriend.js
@@ -0,0 +1,26 @@
+handlers.inviteFriend = function (args, context) 
+{
+	var friendPlayFabID = args.friend
+	
+  	
+	var addFriend = server.AddFriend
+    (
+          {
+              PlayFabId: currentPlayerId,
+              FriendPlayFabId: args.friend
+          }
+    );
+    var addPlayerToFriend = server.AddFriend
+    (
+          {
+              PlayFabId: args.friend,
+              FriendPlayFabId: currentPlayerId
+          }
+    );
+    var sendNotification = server.SendPushNotification
+	    ({
+	        Recipient: args.friend,
+	        Message: "Your friend has joined the game"
+	    });
+      	
+ }

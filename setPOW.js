3fa0429244673f37c63fc070224c683422aeddbf
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-15 14:53:12 +0300

    weekly reward + potw

--- setPOW.js
+++ setPOW.js
@@ -1,24 +1,43 @@
 handlers.setPOTW = function (args, context) 
 {
   var playerID = context.playerProfile.PlayerId;
-  var setPOTWData = server.SetTitleData
-  (
-   		{
-  			Key: "potw",
-          	Value: playerID
-		}
-  );
-  
-  var grantReward = server.AddUserVirtualCurrency
-    ({
-        PlayFabId: playerID,
-        VirtualCurrency: "DM",
-        Amount: potwReward
-    });
-    var sendNotification = server.SendPushNotification
-    ({
-        Recipient: playerID,
-        Message: "Congratulations! you are Player Of the Week "
-    });
-    log.debug(context);
+  var rank = context.TriggeringEventData.Rank;
+  if (rank == 1)
+  {
+		var setPOTWData = server.SetTitleData
+	  (
+	   		{
+	  			Key: "potw",
+	          	Value: playerID
+			}
+	  );
+	  
+	  var grantReward = server.AddUserVirtualCurrency
+	    ({
+	        PlayFabId: playerID,
+	        VirtualCurrency: "DM",
+	        Amount: potwReward
+	    });
+	    var sendNotification = server.SendPushNotification
+	    ({
+	        Recipient: playerID,
+	        Message: "Congratulations! you are Player Of the Week "
+	    });  
+	}
+	else
+	{
+		 var grantReward = server.AddUserVirtualCurrency
+	    ({
+	        PlayFabId: playerID,
+	        VirtualCurrency: "DM",
+	        Amount: weeklyReward
+	    });
+	    var sendNotification = server.SendPushNotification
+	    ({
+	        Recipient: playerID,
+	        Message: "Congratulations! you Ranked in Top 10 this week"
+	    });
+	}
+	  
+	    log.debug(context);
 }

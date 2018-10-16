acd6fcd5a6810f0635e8e22e1f6f5134a71ab70c
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-16 13:00:50 +0300

    newPlayerCounters

--- newPlayer.js
+++ newPlayer.js
@@ -3,6 +3,12 @@ handlers.newPlayer = function (args, context)
   	var currentPlayerID = context.playerProfile.PlayerId;
   	glickoItems = [{Rating: offset}, {RD: factor}, {Vol: vol}];
   	var glickoItems = JSON.stringify(glickoItems);
+  	counters = [];
+	counters.push({wins:0});
+	counters.push({fields:0});
+	counters.push({troops:0});
+	counters.push({cards:0});
+	counters.push({duels:0});
   	var updatePlayerData = server.UpdateUserReadOnlyData
 	(
 		{
@@ -11,7 +17,8 @@ handlers.newPlayer = function (args, context)
 		  	{
                 glicko : glickoItems,
                 tutorial: 0,
-                lastShare: "2018-01-15T11:30:29.527Z"
+                lastShare: "2018-01-15T11:30:29.527Z",
+                PlayerCounters: counters
 	        }
 	     }
 	); 

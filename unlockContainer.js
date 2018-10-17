63364be513ef2524c5c1eadfccfaf3475e18902c
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-17 09:48:43 +0300

    test

--- unlockContainer.js
+++ unlockContainer.js
@@ -14,3 +14,18 @@ handlers.unlockContainer = function (args, context)
   return updateTempLoot(unlockContainerBox,currentPlayerId); 
 }
 //==============================================================================================================================
+handlers.unlockTest = function (args, context) 
+{
+  var containerID = args.containerID;
+
+  //unlock box
+  var unlockContainerBox = server.UnlockContainerInstance
+  (
+   		{
+  			PlayFabId: currentPlayerId,
+  			ContainerItemInstanceId: containerID
+		}
+  );
+  //update temp loot for future rerolls
+  return updateTempLoot(unlockContainerBox,currentPlayerId); 
+}

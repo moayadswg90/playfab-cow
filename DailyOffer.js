4b299c52d5a5d3ba09cf50eaa7b6d6a2cc87cae0
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-21 14:21:00 +0300

    dailyOffer loop fix

--- DailyOffer.js
+++ DailyOffer.js
@@ -3,7 +3,7 @@ handlers.setDailyOffer = function (args, context)
 	var offers = ["500FreeGold","BoxOne25Off","BoxOne50Off","BoxTwo25Off","BoxTwo50Off","DoubleGold2Container50Off","DoubleGold6Container50Off"];
 	var offerOne = offers[Math.floor(Math.random() * offers.length)];
 	var offerTwo;
-	for(i = 5; i < 5; i++)
+	for(i = 0; i < 5; i++)
 	{
 		offerTwo = offers[Math.floor(Math.random() * offers.length)];
 		if (offerTwo != offerOne)

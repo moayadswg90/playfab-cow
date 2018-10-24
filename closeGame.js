29ee75e5959283d6254f09c0f93b6017fa9f312f
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-24 16:03:04 +0300

    new handler to close game for testing purposes

---
+++ closeGame.js
@@ -0,0 +1,75 @@
+handlers.closeGame = function (args)
+{
+	goldEarnedFromPlay = gameReward;
+	pointsEarned = 0;
+  	goldEarnedFromWin = 0;	
+  	today = new Date();
+  	firstGame = false;
+  	firstWin = false;
+  	doubleGold = 0;
+  	
+	var playerOneReadOnlyData = server.GetUserReadOnlyData
+    (
+          {
+              PlayFabId: playerOne.id
+          }
+    );
+    var playerTwoReadOnlyData = server.GetUserReadOnlyData
+    (
+          {
+              PlayFabId: playerTwo.id
+          }
+    );
+    var playerOneData;
+    //var playerTwoData;
+    
+    glickoResult = calculateGlicko(playerReadOnlyData, args.isWon);
+  	glickoItems = [{Rating: glickoResult.ratingResult}, {RD: glickoResult.rdResult}, {Vol: glickoResult.volResult}];
+  	glickoItems = JSON.stringify(glickoItems);
+  	
+  	playerOneData.glickoItems =  glickoItems;
+  	//playerTwoData.glickoItems =  glickoItems;
+  	
+  	if(checkFirstGame(playerOneReadOnlyData))
+  	{
+	  	playerOneData.firstGame = today;
+  	}
+  	if(checkFirstWin(playerOneReadOnlyData))
+  	{
+	  	playerOneData.firstWin = today;
+  	}
+  	var updatePlayerOneData = server.UpdateUserReadOnlyData
+	(
+		{
+			PlayFabId: playerOne.id,
+			Data: 
+			{
+            	playerOneData
+	    	}
+	    }
+	);
+  	
+/*
+  	if(checkFirstGame(playerTwoReadOnlyData))
+  	{
+	  	playerTwoData.firstGame = today;
+  	}
+  	if(checkFirstWin(playerTwoReadOnlyData))
+  	{
+	  	playerTwoData.firstWin = today;
+  	}
+	var updatePlayerTwoData = server.UpdateUserReadOnlyData
+	(
+		{
+			PlayFabId: playerOne.id,
+			Data: 
+			{
+            	playerTwoData
+	    	}
+	    }
+	);
+*/
+  	
+  	
+  
+ }

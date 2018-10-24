225c3737fb82826c13d0bcfd038ba94493d394c2
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-24 18:49:23 +0300

    objects test

--- closeGame.js
+++ closeGame.js
@@ -1,14 +1,39 @@
 handlers.closeGame = function (args, context) 
 {
+	var playerOne = [];
+	playerOne.push({id: currentPlayerId});
+	
+	
 	var playerOneReadOnlyData = server.GetUserReadOnlyData
     (
           {
               PlayFabId: currentPlayerId
           }
     );
-    var playerOneValues = calculateEarnedValues(args.isWon, checkFirstGame(playerOneReadOnlyData), checkFirstWin(playerOneReadOnlyData), doubleGoldCheck(currentPlayerId));
-
+    playerOne.push({firstGame: checkFirstGame(playerOneReadOnlyData)});
+	playerOne.push({firstWin: checkFirstWin(playerOneReadOnlyData)});
+/*
+	
+    //get glickoItems to update rank
+    var playerOneGold = calculateEarnedGold(args.isWon, checkFirstGame(playerOneReadOnlyData), checkFirstWin(playerOneReadOnlyData), doubleGoldCheck(currentPlayerId));
+	//update player data
+	var updatePlayerData = server.UpdateUserReadOnlyData
+	(
+		{
+		  PlayFabId: currentPlayerId,
+		  Data: 
+		  {
+            glicko : glickoItems,
+	        firstWin : today,
+	        firstGame : today 
+	      }
+	    }
+	);
+	//update player stats
+	//grant gold
   	
   	
   	return playerOneValues;
+*/
+	return playerOne;
 }
--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-function calculateEarnedValues(isWon, firstWin, firstGame, doubleGold)
+function calculateEarnedGold(isWon, firstWin, firstGame, doubleGold)
 {
-	var values = {};
-	values["playGold"] = gameReward;
-	values["winGold"] = 0;
-	values["doubleGold"] = 0;
-	values["totalGold"] = 0;
-	values["points"] = 0;
+	var gold = {};
+	gold["playGold"] = gameReward;
+	gold["winGold"] = 0;
+	gold["doubleGold"] = 0;
+	gold["totalGold"] = 0;
 	
 	if (firstGame)
 	{
@@ -24,8 +23,8 @@ function calculateEarnedValues(isWon, firstWin, firstGame, doubleGold)
 	}
 	if (doubleGold)
 	{
-		values["double"] = gold["playGold"] + gold["winGold"];
+		gold["double"] = gold["playGold"] + gold["winGold"];
 	}
-	values["totalGold"] = values["playGold"] + values["winGold"] + values["doubleGold"]
-	return values;
+	gold["totalGold"] = gold["playGold"] + gold["winGold"] + gold["doubleGold"]
+	return gold;
 }

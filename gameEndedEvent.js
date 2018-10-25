b02764152de05a93a8183a8582474228f7a5c494
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-25 12:16:41 +0300

    gameEndedByEvent

---
+++ gameEndedEvent.js
@@ -0,0 +1,43 @@
+function endGameByEvent(playerOneId, playerTwoId, isWon, isDuel, troops, fields, cards)
+{
+	//update player who sent event data
+	var playerOne = {};
+	playerOne["id"] =  playerOneId;
+	var playerTwo = {};
+	playerTwo["id"] =  playerTwoId;
+	
+	var playerTwoWon = 0;
+	if(args.isWon == 0)
+		playerTwoWon = 1;
+	
+	var playerOneReadOnlyData = server.GetUserReadOnlyData
+    (
+          {
+              PlayFabId: playerOne["id"]
+          }
+    );
+    var playerTwoReadOnlyData = server.GetUserReadOnlyData
+    (
+          {
+              PlayFabId: playerTwo["id"]
+          }
+    );
+    
+    //player one gold
+    playerOne["firstGame"] = checkFirstGame(playerOneReadOnlyData)
+    playerOne["firstWin"] = checkFirstWin(playerOneReadOnlyData);
+    playerOne["gold"] = calculateEarnedGold(args.isWon, playerOne["firstWin"], playerOne["firstGame"], doubleGoldCheck(playerOne["id"]));
+    
+    //player two gold
+    playerTwo["firstGame"] = checkFirstGame(playerTwoReadOnlyData)
+    playerTwo["firstWin"] = checkFirstWin(playerTwoReadOnlyData);
+    playerTwo["gold"] = calculateEarnedGold(playerTwoWon, playerTwo["firstWin"], playerTwo["firstGame"], doubleGoldCheck(playerTwo["id"]));
+    
+    //glicko both players
+     glickoResult = calculateGlicko(playerOneReadOnlyData, playerTwoReadOnlyData, args.isWon);
+     playerOne["glicko"] = glickoResult[0];
+     playerTwo["glicko"] = glickoResult[1];
+
+	var result = [playerOne, playerTwo]
+	return result;
+}
--- roomEvent.js
+++ roomEvent.js
@@ -1,6 +1,9 @@
 handlers.RoomEventRaised = function (args) 
 {
 	var eventName = "";
+	var senderId = args.UserId;
+	var playerTwoId;
+	var result;
 	if (args.EvCode == 10)
 	{
 		eventName = "gameStarted";
@@ -15,11 +18,30 @@ handlers.RoomEventRaised = function (args)
 	else if (args.EvCode == 20)
 	{
 		eventName = "gameEnded";
+		eventData = args.Data;
+		var roomData = server.GetSharedGroupData
+		(
+	    	{
+	        	SharedGroupId: photonGameID,
+				GetMembers: true
+	    	}
+		);
+		for (i = 0; i < roomData.Members; i++)
+		{
+			if(roomData.Members[i] != senderId)
+				playerTwoId = roomData.Members[i];
+		}
+		result = endGameByEvent(senderId, playerTwoId, eventData["isWon"], eventData["isDuel"], eventData["troops"], eventData["fields"], eventData["cards"];	
+	}
+	else
+	{
+		//do nothing
 	}
     server.WriteTitleEvent
     (
     	{
-        	EventName : eventName
+        	EventName : eventName,
+        	Body: {result}
     	}
     );
     return { ResultCode : 0, Message: 'Success' };

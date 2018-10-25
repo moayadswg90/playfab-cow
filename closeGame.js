39da01a89283b8e0ba15928cefb836528d32c7ed
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-25 19:52:20 +0300

    s

--- closeGame.js
+++ closeGame.js
@@ -67,7 +67,7 @@ handlers.closeGame = function (args, context)
     (
           {
               SharedGroupId: "test",
-              Data: {playerOne}
+              Data: {playerOne: playerOne}
           }
     );
 	var result = [playerOne, playerTwo]

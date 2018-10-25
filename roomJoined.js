d3c9a639be7c581370a0ea1f7f6b823811f8a4cb
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-25 16:28:41 +0300

    k

--- roomJoined.js
+++ roomJoined.js
@@ -1,19 +1,11 @@
 handlers.RoomJoined = function (args) 
 {
 
-	var playerID = args.UserId;
-	server.WriteTitleEvent
-    (
-	    {
-	        EventName : "room_joined",
-	        Body: {joinerId: playerID}
-	    }
-    );
 	var addRoomCreatorToGroup = server.AddSharedGroupMembers
     (
           {
-              SharedGroupId: photonGameID,
-              PlayFabIds: [playerID]
+              SharedGroupId: args.GameId,
+              PlayFabIds: args.UserId
           }
     );
 

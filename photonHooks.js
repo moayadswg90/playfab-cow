3881566e64ec8e383639e788b3b879b2b5760d17
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-25 11:38:33 +0300

    photonHooks.js orgnize

--- photonHooks.js
+++ photonHooks.js
@@ -1,12 +1,18 @@
 
 
 handlers.RoomPropertyUpdated = function (args) {
+//set cards, duels, troops, fields in sharedGroupData
+
     server.WriteTitleEvent({
         EventName : "room_property_changed"
     });
     return { ResultCode : 0, Message: 'Success' };
 };
-handlers.RoomEventRaised = function (args) {
+handlers.RoomEventRaised = function (args) 
+{
+	//check event code 20 for game is done;
+	//read sharedGroupData property
+	//set player id of winning in sharedGroupData
     server.WriteTitleEvent({
         EventName : "room_event_raised"
     });

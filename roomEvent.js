20394ec35e7f506e84252497bfa3dbfea207e22c
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-25 17:13:11 +0300

    s

--- roomEvent.js
+++ roomEvent.js
@@ -38,12 +38,14 @@ handlers.RoomEventRaised = function (args)
 	    	}
 
 		);
+/*
 		for (i = 0; i < roomData.Members; i++)
 		{
 			if(roomData.Members[i] != args.UserId)
 				playerTwoId = roomData.Members[i];
 		}
 		result = endGameByEvent(args.GameId, args.UserId, playerTwoId, eventData["isWon"], eventData["isDuel"], eventData["troops"], eventData["fields"], eventData["cards"]);
+*/
 
 
 

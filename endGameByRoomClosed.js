ff0e94413b7eb6cde8e10b0bdb6eef83ea0655f4
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-27 15:24:00 +0300

    s

--- endGameByRoomClosed.js
+++ endGameByRoomClosed.js
@@ -99,7 +99,7 @@ handlers.endGameByRoomClosed = function (args)
 		glickoResult = calculateGlicko(playerTwoReadOnlyData, playerOneReadOnlyData, 0);
 		updatePlayer(playerTwoId, false, false, JSON.stringify(glickoResult[0]));
 		updateStats(playerTwoId, 0, parseInt(glickoResult[0]["Rating"]), 0, 0, 0, 0);
-		return parseInt(glickoResult[0]["Rating"];
+		return {result: parseInt(glickoResult[0]["Rating"]};
 	}
 	else if(keys[playerOneId] == null && keys[playerTwoId] != null )
 	{
@@ -129,7 +129,7 @@ handlers.endGameByRoomClosed = function (args)
 	    //glicko both players
 		glickoResult = calculateGlicko(playerOneReadOnlyData, playerTwoReadOnlyData, 0);
 		updatePlayer(playerOneId, false, false, glickoResult[0]);
-		return parseInt(glickoResult[0]["Rating"];
+		return {result: parseInt(glickoResult[0]["Rating"]};
 		updateStats(playerOneId, 0, parseInt(glickoResult[0]["Rating"]), 0, 0, 0, 0);
 	}
 	return {code:1};

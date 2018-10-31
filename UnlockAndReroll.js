1e23bfb96b41a425f65ea7514d9e444592e597ea
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-10-31 11:31:19 +0300

    s

--- UnlockAndReroll.js
+++ UnlockAndReroll.js
@@ -9,7 +9,9 @@ handlers.unlockContainer = function (args, context)
 	  			ContainerItemId: args.containerID
 			}
 	  );
-	  return updateTempLoot(unlockContainerBox,currentPlayerId);
+	  log.debug(JSON.parse(unlockedContainer.GrantedItems[0]))
+	  return {result: JSON.parse(unlockedContainer.GrantedItems[0])};
+	  //return updateTempLoot(unlockContainerBox,currentPlayerId);
   }
   catch(e)
   {

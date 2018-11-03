a8f24dcf02949c69e7b0deff27ea73e2d5b87ef1
Moayad Alsowayegh <alsowayegh@cointoss.studio>
2018-11-03 14:24:03 +0300

    s

--- TriggeredEvents.js
+++ TriggeredEvents.js
@@ -27,3 +27,52 @@ handlers.newPlayer = function (args, context)
 		}
   	);
 }
+handlers.setPOTW = function (args, context) 
+{
+	try
+	{
+		var playerID = context.playerProfile.PlayerId;
+		  var rank = context.playStreamEvent.Rank;
+		  if (rank == 1)
+		  {
+				var setPOTWData = server.SetTitleData
+			  (
+			   		{
+			  			Key: "potw",
+			          	Value: playerID
+					}
+			  );
+			  
+			  var grantReward = server.AddUserVirtualCurrency
+			    ({
+			        PlayFabId: playerID,
+			        VirtualCurrency: "DM",
+			        Amount: potwReward
+			    });
+			    var sendNotification = server.SendPushNotification
+			    ({
+			        Recipient: playerID,
+			        Message: "Congratulations! you are Player Of the Week "
+			    });  
+			}
+			else
+			{
+				 var grantReward = server.AddUserVirtualCurrency
+			    ({
+			        PlayFabId: playerID,
+			        VirtualCurrency: "DM",
+			        Amount: weeklyReward
+			    });
+			    var sendNotification = server.SendPushNotification
+			    ({
+			        Recipient: playerID,
+			        Message: "Congratulations! you Ranked in Top 10 this week"
+			    });
+			}  
+	}
+	catch(e)
+	{
+		return {code: e.apiErrorInfo.apiError.errorCode, error: e.apiErrorInfo.apiError.error};
+	}
+    
+}
